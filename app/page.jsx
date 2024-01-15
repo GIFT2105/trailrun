c0707d2a0df6bb1eb67aea8de2fc900892805047
GIFT2105 <letuka.gift21@gmail.com>
2024-01-15 17:18:00 +0200

    troubleshooting

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 "use client"
 import Image from 'next/image'
 import LoginForm from '../components/login'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="flex h-screen w-screen  flex-col items-center ">
    <h1 id='' className='font-abc  text-4xl font-bold mt-20  text-red-600   '>My Wines App</h1>
     <LoginForm  />
+    <Link href='/gets'>Go to</Link>
     </main>
   )
 }
